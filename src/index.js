817b3e11168b48428b0ae578badc9b412f6fd635
JeronimoCachafeiro <jeronimocachafeiro@gmail.com>
2022-10-17 15:27:08 -0300

    Add properties to todo factory

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import {Todo} from "../src/todo.js"
+
+const todo = Todo("A title", "A description", "Tomorrow", "Top priority");
---
+++ src/todo.js
@@ -0,0 +1,10 @@
+const Todo = (title, description, dueDate, priority) => {
+  return {
+    title: title,
+    description: description,
+    dueDate: dueDate,
+    priority: priority,
+  }
+};
+
+export {Todo};
