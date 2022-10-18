86c0e536126985f928a55ba568ad071b926c8e68
JeronimoCachafeiro <jeronimocachafeiro@gmail.com>
2022-10-18 07:30:27 -0300

    Render todos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import {Todo} from "../src/todo.js"
+import { Todo } from "../src/todo.js";
 import { defaultProject } from "./project";
+import { renderTodos } from "./ui.js";
 
-const todo = Todo("A title", "A description", "Tomorrow", "Top priority");
+const todo1 = Todo("A title", "A description", "Tomorrow", "Top priority");
+todo1.addToProject(defaultProject);
 
+const todo2 = Todo("2 A title", "2 A description", "2 Tomorrow", "2 Top priority");
+todo2.addToProject(defaultProject);
 
-todo.addToProject(defaultProject);
+const todo3 = Todo("3 A title", "3 A description", "3 Tomorrow", "3 Top priority");
+todo3.addToProject(defaultProject);
 console.log(defaultProject);
+
+renderTodos(defaultProject);
---
+++ src/ui.js
@@ -0,0 +1,20 @@
+const content = document.querySelector("#content");
+
+const renderTodos = (project) => {
+  let projectCard = document.createElement("div");
+  projectCard.classList.add("project");
+  for (const todo of project) {
+    const todoCard = document.createElement("div");
+    todoCard.classList.add("todo");
+
+    const title = document.createElement("p");
+    title.classList.add("title");
+    title.textContent = todo.title;
+    todoCard.appendChild(title);
+
+    projectCard.appendChild(todoCard);
+  }
+  content.appendChild(projectCard);
+};
+
+export { renderTodos };
